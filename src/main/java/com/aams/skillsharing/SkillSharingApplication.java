405eaaa6c182a6c0aa266281f8bd19c9385a49a9
mjmmattoni98 <javier.magni@gmail.com>
2022-02-10 22:02:37 +0100

    first commit my panas

---
+++ src/main/java/com/aams/skillsharing/SkillSharingApplication.java
@@ -0,0 +1,13 @@
+package com.aams.skillsharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aams/skillsharing/SkillSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aams.skillsharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
